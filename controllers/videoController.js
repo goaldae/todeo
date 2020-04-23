3a73ec6e5091e8edee85984afe543e2a5152bea8
goaldae <goaldae@naver.com>
2020-04-23 13:06:43 +0900

    Add forms, mixin for videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
-export const home = (req, res) => res.render("home", {pageTitle:"Home"});
+import {videos} from "../db"
+
+export const home = (req, res) => res.render("home", {pageTitle:"Home", videos});
 
 export const search = (req, res) => {
     const {query:{term:searchingBy}} = req; //ES6 문법 const searchingBy = req.query.term; 과 같음
@@ -6,7 +8,7 @@ export const search = (req, res) => {
     res.render("search", {pageTitle:"Search", searchingBy:searchingBy});
 }
 
-export const videos = (req, res) => res.render("videos", {pageTitle:"Videos"});
+
 export const upload = (req, res) => res.render("upload", {pageTitle:"Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"Edit Video"});
