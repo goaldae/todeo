73e78f6216febc76537b1389f5dc2daec7e56ee3
goaldae <goaldae@naver.com>
2020-04-24 11:40:36 +0900

    Add post upload, video detail

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,9 @@ export const getJoin = (req, res) => res.render("join", {pageTitle:"Join"});
 
 export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
 
-export const logout = (req, res) => res.render("logout", {pageTitle:"Logout"});
+export const logout = (req, res) => {
+    res.redirect(routes.home);
+}
 export const users = (req, res) => res.render("users", {pageTitle:"Users"});
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle:"User Detail"});
 export const editPofile = (req, res) => res.render("editProfile", {pageTitle:"Edit Profile"});
