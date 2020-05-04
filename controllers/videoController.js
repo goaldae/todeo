c765c0d56bc01b16f11109663e848d9080549262
goaldae <goaldae@naver.com>
2020-05-04 10:49:48 +0900

    Add video detail, delete controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,50 +1,93 @@
 import routes from "../routes";
-import Video from "../models/Video"
+import Video from "../models/Video";
 
-export const home = async(req, res) => {
-    try {
-        const videos = await Video.find({}); //끝나야 실행한다
-        res.render("home", {pageTitle:"Home", videos});    
-    } catch (error) {
-        console.log(error);
-        res.render("home", {pageTitle:"Home", videos: []}); //videos:[] : 빈배열 일단 할당하기    
-    }
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({}).sort({ _id: -1 }); //끝나야 실행한다
+    //id값으로 정렬하는데 원래 순서에 반대로 정렬한다는 뜻
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] }); //videos:[] : 빈배열 일단 할당하기
+  }
 };
 
 export const search = async (req, res) => {
-    console.log(req);
-    const videos = await Video.find({})
-    const {query:{term:searchingBy}} = req; //ES6 문법 const searchingBy = req.query.term; 과 같음
-    
-    res.render("search", {pageTitle:"Search", searchingBy, videos});
-}
+  console.log(req);
+  const videos = await Video.find({});
+  const {
+    query: { term: searchingBy },
+  } = req; //ES6 문법 const searchingBy = req.query.term; 과 같음
 
-export const getUpload = (req, res) => res.render("upload", {pageTitle:"Upload"});
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
+};
 
-export const postUpload = async(req, res) => {    
-    const{
-        body:{title, description},
-        file:{path}
-    } = req;
-    
-    const newVideo = await Video.create({
-        fileUrl:path,
-        title,
-        description
-    });
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
 
-    res.redirect(routes.videoDetail(newVideo.id));
+export const postUpload = async (req, res) => {
+  const {
+    body: { title, description },
+    file: { path },
+  } = req;
+
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = async (req, res) => {
-    const {
-        params :{id}
-    } = req;
-    
+  const {
+    params: { id },
+  } = req;
+  try {
     const video = await Video.findById(id);
-    console.log(video);    
-    res.render("videoDetail", {pageTitle:"Video Detail", video});
-}
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
-export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"Edit Video"});
-export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle:"Delete Video"});
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: "Edit Video", video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+
+  res.render("editVideo", { pageTitle: "Edit Video" });
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    body: { title, description },
+    params: { id },
+  } = req;
+
+  try {
+    await Video.findByIdAndUpdate({ _id: id }, { title, description }); //모델과 변수명이 똑같아서 ㅇㅇ:ㅇㅇ 형식 생략
+    res.redirect(routes.videoDetail(id));
+  } catch {
+    res.redirect(routes.home);
+  }
+  res.render("editVideo", { pageTitle: "Edit Video" });
+};
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  try {
+    await Video.findByIdAndRemove({ _id: id });
+  } catch (error) {}
+
+  res.redirect(routes.home);
+};
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import {videoDetail, editVideo, deleteVideo, postUpload, getUpload } from "../controllers/videoController";
+import {videoDetail, editVideo, deleteVideo, postUpload, getUpload, getEditVideo, postEditVideo } from "../controllers/videoController";
 import {uploadVideo} from"../middlewares"
 
 const videoRouter = express.Router();
@@ -8,9 +8,12 @@ const videoRouter = express.Router();
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo(), editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
 
