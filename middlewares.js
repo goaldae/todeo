f7b316b83ed422965627dbd730711c2648882629
goaldae <goaldae@naver.com>
2020-05-13 15:42:13 +0900

    Complete CSS

--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,16 @@
 import routes from "./routes";
 import multer from "multer";
 
-const multerVideo = multer({dest:"uploads/videos/"}); //비디오 업로드 경로 설정
+const multerVideo = multer({ dest: "uploads/videos/" }); //비디오 업로드 경로 설정
 
 export const localMiddleware = (req, res, next) => {
-    res.locals.siteName = "Todeo";
-    res.locals.routes = routes;
-    res.locals.user = {
-        id: 2,
-        isAuthenticated: true
-    };
-    next();
-}
+  res.locals.siteName = "Todeo";
+  res.locals.routes = routes;
+  res.locals.user = {
+    id: 2,
+    isAuthenticated: true,
+  };
+  next();
+};
 
 export const uploadVideo = multerVideo.single("videoFile"); //form의 name과 같게, 한개의 파일을 받음
