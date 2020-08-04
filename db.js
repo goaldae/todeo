45c75020245641f97974e54575f2b9ca5e58e5ef
goaldae <goaldae@naver.com>
2020-08-04 12:57:45 +0900

    Connecting MongoDB Atlas replacing local DB

--- db.js
+++ db.js
@@ -2,14 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL_PROD, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-
 const handleOpen = () => console.log("✔ DB connected");
 const handleError = (error) => console.log(`Error on DB connection: ${error}`);
 
